b82424256e7a40d1f005100b05bbdb512e178498
Distant22 <sindistant@gmail.com>
2026-05-23 14:25:04 +0800

    first commit

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+# Ultralytics AGPL-3.0 License - https://ultralytics.com/license
 """
 Train a YOLOv5 model on a custom dataset. Models and datasets download automatically from the latest YOLOv5 release.
 
